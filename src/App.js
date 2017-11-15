23b784c56e900ebf4e463c2d1a865761bfba3319
Emre BELEN <ebelen@netas.lab.nortel.com>
2017-11-15 22:35:15 +0300

    Deleting lines with React.js

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+	{
+		title: 'React',
+		url: 'https://facebook.github.io/react/',
+		author: 'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: 'Redux',
+		url: 'https://github.com/reactjs/redux',
+		author: 'Dan Abramov, Andrew Clark',
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
+
+const person = {
+	name: 'Emre',
+	surname: 'BELEN',
+	age: 26,
+	province: 'İzmir',
+	pic: 'http://tinyurl.com/y83m7fd3',
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list,
+			person,
+		};
+
+		this._onDismiss = this._onDismiss.bind(this);
+	}
+
+	_onDismiss(id) {
+		const updatedList = this.state.list.filter(item => item.objectID !== id);
+		this.setState({ list: updatedList });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{ this.state.list.map(item =>
+					<div key={item.objectID}>
+						<span>
+							<a href={item.url}>{item.title}</a>
+						</span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+						<span>
+							<button 
+								onClick={() => this._onDismiss(item.objectID)} 
+								type="button">
+								Dismiss
+							</button>
+						</span>
+					</div>
+				)
+				}
+				<div>
+					<span><img alt={this.state.person.name} src={this.state.person.pic} height="42" width="42"/>    </span>
+					<span>{this.state.person.name} </span>
+					<span>{this.state.person.surname} </span>
+					<span>{this.state.person.age} </span>
+					<span>{this.state.person.province} </span>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
